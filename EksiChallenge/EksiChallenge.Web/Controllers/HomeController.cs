e2d25b998c2137058faa4fa12b4aae073ed0232a
Ozan Topal <ozantopal@msn.com>
2017-11-28 13:12:51 +0300

    UI'daki gereksinimler tamamlandı.

--- EksiChallenge/EksiChallenge.Web/Controllers/HomeController.cs
+++ EksiChallenge/EksiChallenge.Web/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using EksiChallenge.Common.Models;
+using EksiChallenge.Core.IServices;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,9 +11,35 @@ namespace EksiChallenge.Web.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private IBreweryService service;
+        public HomeController(IBreweryService service)
         {
-            return View();
+            this.service = service;
+        }
+
+        [NonAction]
+        private SelectList GetPageSelectList(int threshold,
+                                             int selected)
+        {
+            var numberList = Enumerable.Range(1, threshold).ToList();
+            var result = new SelectList(numberList, selected);
+
+            return result;
+        }
+
+        public async Task<ActionResult> Index(string searchQuery = "",
+                                              bool isAscending = true,
+                                              int pageNumber = 1)
+        {
+            string apiKey = System.Web.Configuration.WebConfigurationManager.AppSettings["apiKey"];
+            ServiceResponse response = await service.GetBreweries(apiKey, searchQuery, isAscending, pageNumber);
+
+            var pagingSelectList = GetPageSelectList(response.TotalPage, response.CurrentPage);
+            ViewData["PagingList"] = pagingSelectList;
+            ViewData["SearchQuery"] = searchQuery;
+            ViewData["IsAscending"] = isAscending;
+
+            return View(response);
         }
     }
 }
