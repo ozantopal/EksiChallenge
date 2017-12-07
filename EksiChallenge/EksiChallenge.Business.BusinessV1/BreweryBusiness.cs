49423e9d77472b6d5106042e09610e2ea142f084
Ozan Topal <ozantopal@msn.com>
2017-12-07 16:33:02 +0300

    Business katmanına ait geliştirmeleri yap.

---
+++ EksiChallenge/EksiChallenge.Business.BusinessV1/BreweryBusiness.cs
@@ -0,0 +1,25 @@
+﻿using EksiChallenge.Business.Interfaces;
+using EksiChallenge.CrossCutting.Common.Models;
+using EksiChallenge.Repositories.BreweryDbServiceRepository;
+using EksiChallenge.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EksiChallenge.Business.BusinessV1
+{
+    public class BreweryBusiness : IBreweryBusiness
+    {
+        private readonly IRepository<Brewery> breweryRepository;
+        public BreweryBusiness(BreweryRepository breweryRepository)
+        {
+            this.breweryRepository = breweryRepository;
+        }
+        public Task<ServiceResponse<Brewery>> GetBreweries(ServiceParameter parameter)
+        {
+            return this.breweryRepository.Get(parameter);
+        }
+    }
+}
---
+++ EksiChallenge/EksiChallenge.Business.Interfaces/IBreweryBusiness.cs
@@ -0,0 +1,14 @@
+﻿using EksiChallenge.CrossCutting.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EksiChallenge.Business.Interfaces
+{
+    public interface IBreweryBusiness
+    {
+        Task<ServiceResponse<Brewery>> GetBreweries(ServiceParameter parameter);
+    }
+}
